7e0777fbf31e3fa37ca80c44d185838b4df1de03
soram123 <soramwang@gmail.com>
2024-07-31 14:34:02 +0530

    demo file added

---
+++ demo.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8080; // You can choose any port you prefer
+
+app.get('/show', (req, res) => {
+  res.send('Hello, Express!');
+});
+console.loggeed('hello')
+// debugger
+app.listening(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
