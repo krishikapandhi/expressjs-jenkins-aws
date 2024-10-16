be7f974932624b4a9019ee4dabd071fa3969ab44
Krishika Pandhi <143992388+krishikapandhi@users.noreply.github.com>
2024-10-17 02:07:20 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8888; // You can choose any port you prefer
 
+// Add this route for the root path
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express server!');
+});
+
 app.get('/show', (req, res) => {
   res.send('Hello, Express!');
 });
-// console.loggeed('hello')
-// debugger
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
