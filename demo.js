950623a49faa95adad5998511ee40164ddc8e74a
Krishika Pandhi <143992388+krishikapandhi@users.noreply.github.com>
2024-10-17 02:00:49 +0530

    Update demo.js
    using the repository for practical4

--- demo.js
+++ demo.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8888; // You can choose any port you prefer
 
+// Route for root path
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express server!');
+});
+
+// Route for /show
 app.get('/show', (req, res) => {
   res.send('Hello, Express!');
 });
-// console.loggeed('hello')
-// debugger
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
